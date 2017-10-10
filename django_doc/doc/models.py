99bd3be5520bf20795ad0ef36774e4e07fc9776f
janggunhee <jan99unhee@gmail.com>
2017-10-10 22:47:38 +0900

    Person class 추가

---
+++ django_doc/doc/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Person(models.Model):   # person은 실제 database의 table이 된다
+
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    # 클래스 이름 속성으로 지정 각 속성은 데이터 베이스  column 에 맵핑
