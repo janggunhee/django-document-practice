f4051b5f8eac42c5a970df9417853b041bb8e9bc
janggunhee <jan99unhee@gmail.com>
2017-10-10 22:58:30 +0900

    Fruit class 추가

--- django_doc/doc/models.py
+++ django_doc/doc/models.py
@@ -12,3 +12,10 @@ class Person(models.Model):   # person은 실제 database의 table이 된다
     last_name = models.CharField(max_length=30)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
     # 클래스 이름 속성으로 지정 각 속성은 데이터 베이스  column 에 맵핑
+
+
+class Fruit(models.Model):
+    name = models.CharField(
+        max_length=30,
+        primary_key=True
+    )
