3cfdd81ef41de12236536da9643a21897fa34e30
janggunhee <jan99unhee@gmail.com>
2017-10-12 06:24:33 +0900

    recommend_membership 추가

--- django_doc/doc/models/many_to_many/intermediate.py
+++ django_doc/doc/models/many_to_many/intermediate.py
@@ -20,18 +20,31 @@ class Group(models.Model):
     members = models.ManyToManyField(
         Idol,
         through='Membership',
+        through_fields=('group', 'idol'),
     )
+    # Membership 에 있는 group
+    # through, through_fields = Many to Many 두 필드에 선언 ,
+    # 이 곳이 소스 필드 타겟은 'Membershipd class의 group, idol
 
     def __str__(self):
         return self.name
 
 class Membership(models.Model):
-    idol = models.ForeignKey(Idol, on_delete=models.CASCADE)
-    Group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    idol = models.ForeignKey(
+        Idol,
+        on_delete=models.CASCADE,
+        related_name='membership_set'
+    )
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    recommenders = models.ManyToManyField(
+        Idol,
+        blank=True,
+        related_name='recommend_membership_set',
+    )
     joined_date = models.DateField()
     is_active = models.BooleanField()
 
     def __str__(self):
-        return f'{self.group.name} '\
-               f'{self.idol.name} '\
+        return f'{self.group.name} ' \
+               f'{self.idol.name} ' \
                f'({self.is_active})'
